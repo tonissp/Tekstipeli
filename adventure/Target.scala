2ce74fb77faabe12ed68fc7e650807cd6d899831
EkiTheMaaKone <55178907+EkiTheMaaKone@users.noreply.github.com>
2021-11-26 23:32:10 +0200

    Update Target.scala

--- adventure/Target.scala
+++ adventure/Target.scala
@@ -4,19 +4,18 @@ import scala.collection.mutable.Map
 
 class Target(val name: String, world: Adventure, item: Item) {
 
-
+    /** Initial value definitions for the class. */
+  
   private val initialLocation = world.bedroom
-  var location = initialLocation         // gatherer: changes in relation to the old location
+  private val targetPossessions = Map[String, Item]()
   private var isOoC = false                      // flag: can be broken or intact
   private var publicDeathh = false
-  private val targetPossessions = Map[String, Item]()
-  var uhoh = false
-
-  def targetItems = targetPossessions
-
+  var playerInSameSquare = false
   var tPrevLoc = initialLocation
+  var location = initialLocation         // gatherer: changes in relation to the old location
 
-
+  /**Returns the items held by the NPC.  */
+  def targetItems = targetPossessions
 
   /** Returns a square that neighbors the robot's current location in the given direction. */
   def neighbors = this.location.neighbors
@@ -30,7 +29,8 @@ class Target(val name: String, world: Adventure, item: Item) {
     this.location.addItem(item)
     this.isOoC = true
   }
-
+  
+  /** If the target is poisoned this function will be activated. */
   def publicDeath() = {
     world.score += 800
     this.publicDeathh = true
@@ -43,15 +43,15 @@ class Target(val name: String, world: Adventure, item: Item) {
   def isFine = !this.isOoC
   def isPublic = this.publicDeathh
 
-
-
-
-  val routine1 = Vector(world.bedroom, world.workroom, world.bedroom)
-  val routine2 = Vector(world.workroom, world.guestRoom, world.stairs, world.wineCellar, world.wineCellar, world.stairs, world.hallway, world.entrance, world.ballroomSouth, world.ballroomNorth, world.ballroomSouth)
+  /** These lines define the possible routes the target walks along. */
+  
+  val routine1 = Vector(world.bedroom, world.study, world.bedroom)
+  val routine2 = Vector(world.study, world.guestRoom, world.stairs, world.wineCellar, world.wineCellar, world.stairs, world.hallway, world.entrance, world.ballroomSouth, world.ballroomNorth, world.ballroomSouth)
   val routine3 = Vector(world.wc, world.toiletRoom, world.toiletRoom, world.toiletRoom, world.wc)
-  val routine4 = Vector(world.ballroomSouth, world.ballroomNorth, world.ballroomSouth, world. entrance, world.hallway, world.stairs, world.guestRoom, world.workroom)
+  val routine4 = Vector(world.ballroomSouth, world.ballroomNorth, world.ballroomSouth, world. entrance, world.hallway, world.stairs, world.guestRoom, world.study)
 
 
+  /** Makes the target pick up something. */
   def targetGet(itemName: String) = {
     val received = this.location.removeItem(itemName)
     for (newItem <- received) {
@@ -59,11 +59,11 @@ class Target(val name: String, world: Adventure, item: Item) {
     }
   }
 
-
+  /** Returns the message displayed when in the same area as the target. */
   def sameSpot = if(!this.world.guests.areas.contains(this.location) && this.isFine) {
-  uhoh = true
+  playerInSameSquare = true
   "'What the hell are you doing here!'"
-  } else if(!this.isFine) "There's your target. Laying dead. Good job." else "You see your target. All that you need now is a moment alone with him."
+  } else if(!this.isFine) "There's your target laying dead. Good job. Now you just need to get out of here." else "You see your target. All that you need now is a moment alone with him."
 
   override def toString: String = sameSpot
 
