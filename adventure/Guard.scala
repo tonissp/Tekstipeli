502e778e0615518b69ad869e03143c902dde4af6
EkiTheMaaKone <55178907+EkiTheMaaKone@users.noreply.github.com>
2021-11-27 00:33:47 +0200

    Update Guard.scala

--- adventure/Guard.scala
+++ adventure/Guard.scala
@@ -9,39 +9,22 @@ class Guard(val name: String, world: Adventure, item: Item) {
 
 
 
-  /** Returns a square that neighbors the robot's current location in the given direction. */
+  /** Returns a square that neighbors the NPC's current location in the given direction. */
   def neighbors = this.location.neighbors
 
 
 
-  /** Causes the robot to malfunction (typically as the result of a collision).
-    * A broken robot does not do anything during its turns.
-    * @see [[fix]] */
+  /** Used to incapacitate the guard */
   def incapacitateG() = {
     this.location.addItem(item)
     this.isOoC = true
   }
 
-  /** Determines whether the robot is currently intact or not. A robot is intact
-    * unless it has been broken with the `destroy` method and not fixed since. */
+  /** Determines whether the guard is currently incapacitated or not.*/
   def isFine = !this.isOoC
 
-
-  /** Relocates the robot within its current world to the square next to the robot's
-    * current location, in the given direction. The direction does not necessarily have
-    * to be the same one that the robot is originally facing in.
-    *
-    * This method turns the robot to face in the direction it moves in.
-    *
-    * Two robots can never be in the same square; neither can a robot and a wall. If the
-    * robot's would-be location is not empty, a collision occurs instead and the robot
-    * does not change locations (but still turns to face whatever it collided with).
-    *
-    * If the moving robot collides with a wall, the robot itself breaks. If a moving robot
-    * collides with another robot, the other robot breaks and the moving robot stays intact.
-    *
-    * @return `true` if the robot successfully changed locations, `false` if it
-    *         did not (even if it changed facing) */
+  /** Used to define the walking pattern of the guard */
+  
   val routine: Vector[Area] = Vector(world.yardEntrance, world.thickGardenEast, world.thickGardenWest, world.frontYard)
 
 
